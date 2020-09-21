8af05a1479aef75bb4eb25b1ba6e0dd79c51f79a
Grigore Stefan <g_stefan@yahoo.com>
2020-09-22 01:34:09 +0300

    update

--- port/build.ubuntu.clean-local-release.sh
+++ port/build.ubuntu.clean-local-release.sh
@@ -3,7 +3,6 @@
 # http://unlicense.org/
 # Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo "-> clean html-to-rc"
+echo "-> clean-local-release html-to-rc"
 
-rm -rf bin
-rm -rf build
+rm -rf release
--- source/html-to-rc-version.hpp
+++ source/html-to-rc-version.hpp
@@ -10,10 +10,10 @@
 #ifndef HTML_TO_RC_VERSION_HPP
 #define HTML_TO_RC_VERSION_HPP
 
-#define HTML_TO_RC_VERSION_ABCD                1,1,0,3
-#define HTML_TO_RC_VERSION_STR                 "1.1.0"
-#define HTML_TO_RC_VERSION_STR_BUILD           "3"
-#define HTML_TO_RC_VERSION_STR_DATETIME        "2020-09-14 00:18:23"
+#define HTML_TO_RC_VERSION_ABCD                1,2,0,4
+#define HTML_TO_RC_VERSION_STR                 "1.2.0"
+#define HTML_TO_RC_VERSION_STR_BUILD           "4"
+#define HTML_TO_RC_VERSION_STR_DATETIME        "2020-09-21 17:38:56"
 
 #ifndef XYO_RC
 
