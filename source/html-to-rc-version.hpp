8d5ce4ee650f77fc3025682cd31a90dac7eb9f34
Grigore Stefan <g_stefan@yahoo.com>
2021-01-03 02:32:28 +0200

    version

--- source/html-to-rc-version.hpp
+++ source/html-to-rc-version.hpp
@@ -10,10 +10,10 @@
 #ifndef HTML_TO_RC_VERSION_HPP
 #define HTML_TO_RC_VERSION_HPP
 
-#define HTML_TO_RC_VERSION_ABCD                1,2,0,5
-#define HTML_TO_RC_VERSION_STR                 "1.2.0"
-#define HTML_TO_RC_VERSION_STR_BUILD           "5"
-#define HTML_TO_RC_VERSION_STR_DATETIME        "2020-12-01 21:07:55"
+#define HTML_TO_RC_VERSION_ABCD                1,4,0,6
+#define HTML_TO_RC_VERSION_STR                 "1.4.0"
+#define HTML_TO_RC_VERSION_STR_BUILD           "6"
+#define HTML_TO_RC_VERSION_STR_DATETIME        "2021-01-03 02:30:47"
 
 #ifndef XYO_RC
 
