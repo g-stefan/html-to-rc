efdf3d651bf12b49e9cd5d1769047124f331b644
Grigore Stefan <g_stefan@yahoo.com>
2021-07-24 19:34:32 +0300

    update

--- source/html-to-rc-version.hpp
+++ source/html-to-rc-version.hpp
@@ -10,10 +10,10 @@
 #ifndef HTML_TO_RC_VERSION_HPP
 #define HTML_TO_RC_VERSION_HPP
 
-#define HTML_TO_RC_VERSION_ABCD                1,6,0,8
-#define HTML_TO_RC_VERSION_STR                 "1.6.0"
-#define HTML_TO_RC_VERSION_STR_BUILD           "8"
-#define HTML_TO_RC_VERSION_STR_DATETIME        "2021-07-06 15:11:43"
+#define HTML_TO_RC_VERSION_ABCD                1,7,0,10
+#define HTML_TO_RC_VERSION_STR                 "1.7.0"
+#define HTML_TO_RC_VERSION_STR_BUILD           "10"
+#define HTML_TO_RC_VERSION_STR_DATETIME        "2021-07-23 16:34:01"
 
 #ifndef XYO_RC
 
