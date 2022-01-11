1080ff53d56112de98801ecaf12c54e15b89ec28
Grigore Stefan <g_stefan@yahoo.com>
2022-01-12 00:56:08 +0200

    Update

--- source/html-to-rc-version.hpp
+++ source/html-to-rc-version.hpp
@@ -10,10 +10,10 @@
 #ifndef HTML_TO_RC_VERSION_HPP
 #define HTML_TO_RC_VERSION_HPP
 
-#define HTML_TO_RC_VERSION_ABCD                1,11,0,23
-#define HTML_TO_RC_VERSION_STR                 "1.11.0"
-#define HTML_TO_RC_VERSION_STR_BUILD           "23"
-#define HTML_TO_RC_VERSION_STR_DATETIME        "2022-01-01 21:19:33"
+#define HTML_TO_RC_VERSION_ABCD                1,13,0,29
+#define HTML_TO_RC_VERSION_STR                 "1.13.0"
+#define HTML_TO_RC_VERSION_STR_BUILD           "29"
+#define HTML_TO_RC_VERSION_STR_DATETIME        "2022-01-09 01:13:04"
 
 #ifndef XYO_RC
 
