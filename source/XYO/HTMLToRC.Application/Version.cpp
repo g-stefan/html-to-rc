879c9b6f2e899b157bd66e0dd2677da7bab1f4b8
Grigore Stefan <g_stefan@yahoo.com>
2024-01-02 22:49:55 +0200

    Update

--- source/XYO/HTMLToRC.Application/Version.cpp
+++ source/XYO/HTMLToRC.Application/Version.cpp
@@ -1,5 +1,5 @@
 // HTML To RC
-// Copyright (c) 2007-2023 Grigore Stefan <g_stefan@yahoo.com>
+// Copyright (c) 2007-2024 Grigore Stefan <g_stefan@yahoo.com>
 // MIT License (MIT) <http://opensource.org/licenses/MIT>
 // SPDX-FileCopyrightText: 2007-2023  Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: MIT
